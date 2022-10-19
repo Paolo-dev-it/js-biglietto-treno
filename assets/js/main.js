f5614b509f4ec8793a86cd715e1ab6970bf4be76
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-19 16:09:30 +0200

    Added  istructions, variables and comments. Project completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,20 +8,48 @@
 
 function totalPriceTravel(){
 
-    const kilometers = 0.21
+    // Costante prezzo chilometri
+
+    const kilometers = 0.21;
+
+    // Variabile numero chilometri che deve fare l'utente
 
     let valueKm = document.getElementById('numbersKm').value;
 
-    document.getElementById('totalKm').innerHTML = `Il costo totale del tuo percorso è : ${valueKm * kilometers}`
+    // Variabile età che ha l'utente
 
     let valueAge = document.getElementById('age').value;
 
-    
-    // let under = 20%
+    // Calcolo costo senza sconti al chilometro
 
-    // if (under ) {
+    let price = valueKm * kilometers;
 
-    // }
+    // Calcolo costo sconto minorenne al chilometro
 
-    // let over = 40%
+    let under = price * 0.2;
+
+    // Calcolo costo sconto anziano al chilometro
+
+    let over = price * 0.4;
+
+    // Variabili costanti prezzo al chilometro compreso di sconto minorenne e anziano
+
+    const childPrice = price - under;
+
+    const seniorPrice = price - over;
+
+    if (valueAge < 18) {
+        price = childPrice.toFixed(2)
+    }
+    else if (valueAge >= 65) {
+        price = seniorPrice.toFixed(2)
+    } 
+    else {
+        price = price.toFixed(2)
+    }
+
+    document.getElementById('totalKm').innerHTML = "Il costo totale del tuo percorso:" + " " + price + "€"
+
+
+
 }
